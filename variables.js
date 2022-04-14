51757484f1c81056271ff496909c7e8e428dcc0a
Shauna MacFarlane <shauna.macfarlane579@gmail.com>
2022-04-13 21:34:19 -0500

    Add annotation

--- variables.js
+++ variables.js
@@ -3,7 +3,9 @@ var hobbies = "Reading, practicing yoga, playing the cello"
 var numberPets = 6
 var numberChildren = 3
 var birthYear = 1976
+//variable dailyExercise is declared and assigned to the boolean of "false"
 var dailyExercise = false
-var drinkWater = 'true'
-dailyExercise
-var dailyExercise = "true"
+//variable drinkWater is declared and assigned to the boolean of "true"
+var drinkWater = true
+
+console.log(name,hobbies,numberPets,numberChildren,birthYear,dailyExercise,drinkWater)
