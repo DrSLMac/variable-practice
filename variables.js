77ecea7597549a0833c93174d3c9cb253dc534f0
Shauna MacFarlane <shauna.macfarlane579@gmail.com>
2022-04-13 21:27:21 -0500

    Add boolean

--- variables.js
+++ variables.js
@@ -3,3 +3,7 @@ var hobbies = "Reading, practicing yoga, playing the cello"
 var numberPets = 6
 var numberChildren = 3
 var birthYear = 1976
+var dailyExercise = false
+var drinkWater = 'true'
+dailyExercise
+var dailyExercise = "true"
